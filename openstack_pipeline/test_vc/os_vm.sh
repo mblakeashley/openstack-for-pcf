265762212f1359cc7392768e29fb2325d1d6f595
mblakeashley <m.blakeashley@gmail.com>
2017-05-18 11:27:08 -0400

    add sudo

--- openstack_pipeline/test_vc/os_vm.sh
+++ openstack_pipeline/test_vc/os_vm.sh
@@ -19,7 +19,7 @@ export GOVC_URL="https://$USERNAME:$PASSWORD@vcsa-01.haas-59.pez.pivotal.io/sdk"
 export GOVC_DATACENTER=Datacenter
 export GOVC_INSECURE=true
 
-govc datacenter.info
+sudo govc datacenter.info
 
 
 # Create Base VM's for OpenStack IaaS
