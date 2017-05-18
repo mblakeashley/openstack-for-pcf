59638f5ec16b2a5875fe2e90bec31a12f6d4233c
mblakeashley <m.blakeashley@gmail.com>
2017-05-18 14:29:21 -0400

    force install

--- openstack_pipeline/test_vc/os_vm.sh
+++ openstack_pipeline/test_vc/os_vm.sh
@@ -12,7 +12,7 @@ URL_TO_BINARY=https://github.com/vmware/govmomi/releases/download/v0.13.0/govc_l
 
 # Update and install Dependencies
 #apt-get update && apt-get install wget -y
-yum update && yum install wget
+yum update && yum install wget -y
 wget $URL_TO_BINARY
 gzip -d govc_linux_amd64.gz | cp govc_linux_amd64 /usr/local/bin/govc
 chmod +x /usr/local/bin/govc
