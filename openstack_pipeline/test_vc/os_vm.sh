f23dfbd672d7534ce66de2a07f5cf493bb388d91
mblakeashley <m.blakeashley@gmail.com>
2017-05-18 10:22:04 -0400

    correct params and depends

--- openstack_pipeline/test_vc/os_vm.sh
+++ openstack_pipeline/test_vc/os_vm.sh
@@ -5,12 +5,17 @@ set -x
 # Script Variables
 URL_TO_BINARY=https://github.com/vmware/govmomi/releases/download/v0.14.0/govc_linux_amd64.gz
 
+# Update and install Dependencies
+apt-get update && apt-get install wget -y
+wget $URL_TO_BINARY
+gzip -d govc_linux_amd64.gz | cp govc_linux_amd64 /usr/local/bin/govc
+chmod +x /usr/local/bin/govc
 
 # Set SSH Keys
 #\cp  ../../id_rsa.pub ~/.ssh/
 
 #Test govc login
-export GOVC_URL='https://$username:$password@vcsa-01.haas-59.pez.pivotal.io/sdk'
+export GOVC_URL='https://$vc-28-username:$vc-28-password@vcsa-01.haas-59.pez.pivotal.io/sdk'
 export GOVC_DATACENTER=Datacenter
 export GOVC_INSECURE=true
 
