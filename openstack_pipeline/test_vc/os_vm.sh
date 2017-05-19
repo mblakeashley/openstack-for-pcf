35abffa41b6c1f3d8d43670ee604c5a47f9e946e
mblakeashley <m.blakeashley@gmail.com>
2017-05-19 14:45:48 -0400

    cleanup script and add exit code

--- openstack_pipeline/test_vc/os_vm.sh
+++ openstack_pipeline/test_vc/os_vm.sh
@@ -1,30 +1,28 @@
 #!/bin/bash -x
-#Dependencies for task "openstack_vm_bootstrap"
-uname -a
-echo $PATH
-
-# Script Variables
+##Dependencies for task "openstack_vm_bootstrap"
 URL_TO_BINARY=https://github.com/vmware/govmomi/releases/download/v0.14.0/govc_linux_amd64.gz
 
 
-# Update and install Dependencies
-yum update && yum install wget -y
-wget $URL_TO_BINARY
-gzip -d govc_linux_amd64.gz 
-mv govc_linux_amd64 /usr/local/bin/govc
-chmod +x /usr/local/bin/govc
+## Update and install Dependencies
+if [[ $(yum list installed | grep "wget") == '' ]] ;
+   then yum update && yum install wget -y
+else wget $URL_TO_BINARY;
+     gzip -d govc_linux_amd64.gz;
+     mv govc_linux_amd64 /usr/local/bin/govc;
+     chmod +x /usr/local/bin/govc
+fi
 
 
-# Set SSH Keys
-#\cp  ../../id_rsa.pub ~/.ssh/
-
-
-#Test govc login
+## Test govc login
+# Export govc Variables
 export GOVC_URL="https://$USERNAME:$PASSWORD@vcsa-01.haas-59.pez.pivotal.io/sdk"
 export GOVC_DATACENTER=Datacenter
 export GOVC_INSECURE=true
-govc datacenter.info
+
+if [[ $govc about.cert | grep "vcsa-01.haas-59.pez.pivotal.io") == '' ]] ;
+   then exit 1
+  else echo "Connection to vSphere.. Success!"
+fi
 
 
-# Create Base VM's for OpenStack IaaS
-#govc vm.create  -annotation=gss-lab-29-os-controller -pool=RP28 -c=16 -m=91024 -net=env28
+## Create Base VM's for OpenStack IaaS
