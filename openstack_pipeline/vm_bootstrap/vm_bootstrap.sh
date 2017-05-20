609c1f05111f60d268f1ffb378c94a601a018933
mblakeashley <m.blakeashley@gmail.com>
2017-05-20 11:24:42 -0400

    fix if statement

--- openstack_pipeline/vm_bootstrap/vm_bootstrap.sh
+++ openstack_pipeline/vm_bootstrap/vm_bootstrap.sh
@@ -3,7 +3,7 @@
 URL_TO_BINARY=https://github.com/vmware/govmomi/releases/download/v0.14.0/govc_linux_amd64.gz
 
 ## Update and install Dependencies
-if [[ $(yum list installed | grep "wget") == '' ]] ;
+if ! [[ $(yum list installed | grep "wget") == '' ]] ;
    then yum update && yum install wget -y;
         wget $URL_TO_BINARY;
         gzip -d govc_linux_amd64.gz;
@@ -22,7 +22,7 @@ export GOVC_URL="https://$USERNAME:$PASSWORD@vcsa-01.haas-59.pez.pivotal.io/sdk"
 export GOVC_DATACENTER=Datacenter
 export GOVC_INSECURE=true
 
-if [[ $(govc about.cert | grep "vcsa-01.haas-59.pez.pivotal.io") == '' ]] ;
+if ! [[ $(govc about.cert | awk -F: '{print $1}' | grep "vcsa-01.haas-59.pez.pivotal.io")  ]] ;
    then "Connection to vSphere.. Success!"
  else exit 1
 fi
