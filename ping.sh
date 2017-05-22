a5a31be36fb835b1fa18c10ded3e3ac8f5e15dd8
mblakeashley <m.blakeashley@gmail.com>
2017-05-22 17:00:36 -0400

    adding ping script

---
+++ ping.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+while getopts ":n:h:" opt; do
+    case ${opt} in
+        n)
+          name=$OPTARG
+          ;;
+        h )
+          host=$OPTARG
+          ;;
+        : )
+          echo "Invalid option: $OPTARG requires an argument" 1>&2
+          ;;
+     esac
+done
+
+if ! ping -c 1 ${name}
+     then
+         echo "Wating on ${host}, sleep for 2 minutes";
+         sleep 2m
+else
+     echo "${host} is ready!";
+     exit 0
+fi
