7ae3d74787c2289f3fd8f5f0c3ff6ae46ef3f2e9
mblakeashley <m.blakeashley@gmail.com>
2017-06-08 16:37:17 -0400

    restructure scripts for pipeline

--- deploy_packstack_packages.sh
+++ deploy_packstack_packages.sh
@@ -5,7 +5,7 @@ source /opt/openstack-for-pcf/deploy_functions.sh
 # Update and Install Dependencies
 step "+ Adding Repos"
     try silent rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org;
-		           rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+		try silent rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 
 step "+ Running YUM Update"
 	  try silent yum update -y -q -e 0
@@ -13,13 +13,13 @@ next
 
 step "+ Installing Base Packages and Kernel Update Version"
 	  try silent yum --enablerepo=elrepo-kernel install -y -q -e 0 vim net-tools tigervnc-server install kernel-ml;
-              grub2-set-default 0;
-              grub2-mkconfig -o /boot/grub2/grub.cfg
+    try silent grub2-set-default 0;
+    try silent grub2-mkconfig -o /boot/grub2/grub.cfg
 
 next
 
-step "+ Installing KDE Desktop.. This will take some time"
-	  try silent yum groupinstall "KDE Plasma Workspaces"  -y -q -e 0;
-              systemctl set-default graphical.target;
-              systemctl isolate graphical.target
-next
+#step "+ Installing KDE Desktop.. This will take some time"
+#	  try silent yum groupinstall "KDE Plasma Workspaces"  -y -q -e 0;
+#              systemctl set-default graphical.target;
+#              systemctl isolate graphical.target
+#next
