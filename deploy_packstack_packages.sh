cfbeeb2e41ad0c9f2dc1335eef81b82f17699d4f
mblakeashley <m.blakeashley@gmail.com>
2017-06-09 11:24:54 -0400

    restructure scripts for pipeline

--- deploy_packstack_packages.sh
+++ deploy_packstack_packages.sh
@@ -1,25 +1,49 @@
 #!/bin/sh
 # This script is intened to install all dependencies pertaining to the openstack-for-pcf project
 source /opt/openstack-for-pcf/deploy_functions.sh
+USER=stack
+
+
+# Setup guest accounts for VNC
+step "+ Checking user accounts"
+next
+if [[ $(cat /etc/passwd | awk -F: '{print $1}' | grep "stack") == '' ]] ;
+   then step "+ Created new user *$USER*" ;
+                try useradd stack ;
+                try spinner passwd stack;
+                echo "stack ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    else echo "Users are Defined!!"
+fi
+next
 
 # Update and Install Dependencies
 step "+ Adding Repos"
-    try silent rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org;
-		try silent rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+        try silent rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org ;
+		    try silent rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 
 step "+ Running YUM Update"
-	  try silent yum update -y -q -e 0
+	      try silent yum update -y -q -e 0
 next
 
 step "+ Installing Base Packages and Kernel Update Version"
-	  try silent yum --enablerepo=elrepo-kernel install -y -q -e 0 vim net-tools tigervnc-server install kernel-ml;
-    try silent grub2-set-default 0;
-    try silent grub2-mkconfig -o /boot/grub2/grub.cfg
+	      try silent yum --enablerepo=elrepo-kernel install -y -q -e 0 vim net-tools tigervnc-server xorg-x11-fonts-Type1 kernel-ml;
+        try silent grub2-set-default 0;
+        try silent grub2-mkconfig -o /boot/grub2/grub.cfg
 
+step "+ Configure VNCserver"
+        try silent cp /opt/openstack-for-pcf/vnc_service /etc/systemd/system/vncserver@:3.service ;
+        try silent mkdir -p /home/stack/.vnc/ ;
+        try silent cp /opt/openstack-for-pcf/vnc_stack /home/stack/.vnc/passwd ;
+        try silent chmod 600 /home/stack/.vnc/passwd
 next
 
-#step "+ Installing KDE Desktop.. This will take some time"
-#	  try silent yum groupinstall "KDE Plasma Workspaces"  -y -q -e 0;
-#              systemctl set-default graphical.target;
-#              systemctl isolate graphical.target
-#next
+step "+ Start VNCserver"
+        try silent systemctl daemon-reload ;
+        try silent systemctl start vncserver@:3.service ;
+        try silent systemctl enable vncserver@:3.service
+next
+
+step "+ Installing KDE Desktop.. This will take some time"
+	      try silent yum groups mark convert;
+	      try silent yum groupinstall "GNOME Desktop" -y -q -e 0;
+next
